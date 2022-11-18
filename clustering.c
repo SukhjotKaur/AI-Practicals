4c1ddb01d05ecbb3f425fefc74c7686d5ca5032e
SukhjotKaur <110758985+SukhjotKaur@users.noreply.github.com>
2022-11-18 12:14:32 +0530

    Update clustering.c

--- clustering.c
+++ clustering.c
@@ -6,7 +6,7 @@ float mean(float arr[], int n)
     float mean, sum = 0;
     for (i = 0; i < n; i++)
     {
-        if(arr[i]!=0){
+        if(arr[i]!=-1){
         sum += arr[i];
             count++;
         }
@@ -40,11 +40,10 @@ int main()
     scanf("%d%d%d%d", &a, &b, &c, &d);
     float hold1, hold2, hold3, hold4;
     hold1 = a, hold2 = b, hold3 = c, hold4 = d;
-    printf("Enter the no of elemnts in the array: ");
+    printf("Enter the no of elements in the array: ");
     scanf("%d", &n);
     printf("Enter the upper and lower limit of the range: ");
     scanf("%d%d", &lower,&upper);
-    // float g1 = a*1.0, g2 = b*1.0, g3 = c*1.0, g4 = d*1.0;
     int arr[n];
     float g1[n/2], g2[n/2], g3[n/2], g4[n/2];
     for (i = 0; i < n; i++)
@@ -56,10 +55,10 @@ int main()
         float *s1 = (float *)malloc((2*n) * sizeof(float)), *s2 = (float *)malloc((2*n) * sizeof(float)), *s3 = (float *)malloc((2*n) * sizeof(float)), *s4 = (float *)malloc((2*n) * sizeof(float));
         for (o = 0; o < (2*n); o++)
         {
-            s1[o] = 0.0;
-            s2[o] = 0.0;
-            s3[o] = 0.0;
-            s4[o] = 0.0;
+            s1[o] = -1.0;
+            s2[o] = -1.0;
+            s3[o] = -1.0;
+            s4[o] = -1.0;
         }
         s1[0] = hold1, s2[0] = hold2, s3[0] = hold3, s4[0] = hold4;
         for (i = 0; i < n; i++)
@@ -91,18 +90,12 @@ int main()
                 k = mean(s4, n);
                 s4[0] = k;
             }
-            // g1 = s1[0], g2 = s2[0], g3 = s3[0], g4 = s3[0];
         }
         hold1 = s1[0], hold2 = s2[0], hold3 = s3[0], hold4 = s4[0];
-
         g1[j] = s1[0];
         g2[j] = s2[0];
         g3[j] = s3[0];
         g4[j] = s4[0];
-        if(g1[j-1]>=0.001*g1[j]||g2[j-1]>=0.001*g2[j]||g2[j-1]>=0.001*g2[j]||g2[j-1]>=0.001*g2[j]||g1[j-1]<=0.001*g1[j]||g2[j-1]<=0.001*g2[j]||g3[j-1]<=0.001*g3[j]||g4[j-1]<=0.001*g4[j])
-        {
-            break;
-        }
         free(s1);
         free(s2);
         free(s3);
